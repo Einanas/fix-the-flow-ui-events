96d4ec179d170f56f49c28a40cb48de47b3fc57e
Einanas <inez.el.azzouzi@hva.nl>
2023-12-13 01:14:49 +0100

    adding frontend and design in js and css

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,27 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+
+// (1) frontend interaction
+let frontend = document.querySelector('a:nth-of-type(1)')
+
+frontend.addEventListener('click', scale)
+frontend.addEventListener('animationend', scale)
+
+function scale() {
+  frontend.classList.toggle('scale')
+}
+
+
+
+// // (2) design interaction
+let design = document.querySelector('a:nth-of-type(2)')
+
+design.addEventListener('click', rotate)
+design.addEventListener('animationed', rotate )
+
+design.classList.toggle('rotate')
+
+console.log('a:nth-of-type(2)');
+
